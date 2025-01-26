b73d84a2c9f96a731d0277be06d48492a3dc50c4
Gabriel Gomes <gabegomes2020@gmail.com>
2025-01-26 02:53:20 -0300

    ssg

--- app/planet/[name]/page.js
+++ app/planet/[name]/page.js
@@ -2,17 +2,29 @@
 
 import React from 'react';
 
+// Função para gerar os parâmetros estáticos
+export async function generateStaticParams() {
+    const apiUrl = `https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=JSON&select=kepid,kepoi_name,kepler_name,koi_disposition,koi_period,koi_prad,koi_teq,koi_insol,koi_steff&where=koi_disposition%20like%20%27CONFIRMED%27`;
+    
+    const planets = await fetch(apiUrl)
+        .then((res) => res.json());
+
+    // Retorna os parâmetros necessários para as rotas dinâmicas
+    return planets.map((planet) => ({
+        name: planet.kepler_name,
+    }));
+}
+
+// Resto do seu código de página
 export default async function PlanetPage({ params }) {
     const { name } = params; // Pegando o parâmetro dinâmico da rota
 
-    // Realiza a busca do planeta usando o nome
     const planet = await fetch(
         `https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&format=JSON&select=kepoi_name,kepler_name,koi_period,koi_prad&where=koi_disposition%20like%20'CONFIRMED'%20AND%20kepler_name%20like%20%27%25${name}%25%27`
     )
         .then((res) => res.json())
-        .then((data) => data[0]); // Assumindo que o primeiro resultado é o mais relevante
+        .then((data) => data[0]);
 
-    // Caso não encontre o planeta
     if (!planet) {
         return (
             <div className="bg-[#0A1D2D] text-white min-h-screen flex flex-col items-center justify-center">
@@ -22,7 +34,6 @@ export default async function PlanetPage({ params }) {
         );
     }
 
-    // Exibindo as informações do planeta
     return (
         <div className="bg-[#0A1D2D] text-white min-h-screen p-6">
             <h1 className="text-4xl font-bold mb-4">{planet.kepler_name}</h1>
